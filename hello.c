34cc0d1cb581cf1d5e33649c8c2b1d240b1b8029
kharwarshreya26 <shreyakharwar33@gmail.com>
2025-08-22 23:18:12 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+BCA-Projects/
+├── C-Programs/
+│   ├── hello.c
+│   └── calculator.c
+├── Java-Programs/
+│   ├── HelloWorld.java
+│   └── Student.java
+└── Python-Scripts/
+    ├── hello.py
+    └── calculator.py
